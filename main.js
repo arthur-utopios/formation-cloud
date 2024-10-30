0aa4fb760f59cdf3f2f5b08bf2d7c4a1a7b58b6e
GeraldineGrenade <geraldine.grenade@gmail.com>
2024-10-30 15:16:44 +0100

    import et app.use routes main.js

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+
 // import routes from './app/routes/routes';
+import paasRouter from './app/routes/paas';
+import saasRouter from './app/routes/saas';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 // app.use(routes);
+app.use('/paas', paasRouter);
+app.use('/saas', saasRouter);
 
 app.get('/', (req, res) => {
     res.json({ status: 'API is running on /api' });
@@ -19,3 +25,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.info(`server up on port ${PORT}`);
 });
+
+export default app;
